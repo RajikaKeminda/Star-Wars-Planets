1491d85b4ea4704db9b3f0ef2612371a92138c2c
Rajika keminda <rkeminda0@gmail.com>
2025-03-20 05:29:40 +0530

    feat: Planet Data model

---
+++ app/src/main/java/com/example/starwarsplanets/data/models/Planet.kt
@@ -0,0 +1,17 @@
+package com.example.starwarsplanets.data.models
+
+
+data class PlanetResponse(
+    val count: Int,
+    val next: String?,
+    val previous: String?,
+    val results: List<Planet>
+)
+
+data class Planet(
+    val name: String,
+    val climate: String,
+    val gravity: String,
+    val url: String,
+    val imageUrl: String,
+)
